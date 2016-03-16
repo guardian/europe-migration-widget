e9b4fb9f6d06002494cd29650739dc26302f512e
Garry Blight <garry.blight@guardian.co.uk>
2016-03-16 14:40:51 +0000

    Data from spreadsheet

--- src/js/embed.js
+++ src/js/embed.js
@@ -8,9 +8,15 @@ window.init = function init(el, config) {
     el.innerHTML = embedHTML;
 
     reqwest({
-        url: 'http://ip.jsontest.com/',
+        url: 'https://interactive.guim.co.uk/docsdata/1bRz4W9fo4IFrdwq8gj44H77tdZtvyJ9k_LJt614scpg.json',
         type: 'json',
         crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
+        success: (resp) => buildView( resp )
     });
 };
+
+// success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
+
+function buildView ( data ) {
+    console.log( data );
+}
