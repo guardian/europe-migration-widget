1f68f5f4fef677a1c563ab2fe2fc76e711c72973
Garry Blight <garry.blight@guardian.co.uk>
2016-03-24 17:09:11 +0000

    Basic graph script incorporated

--- src/js/embed.js
+++ src/js/embed.js
@@ -39,7 +39,7 @@ function buildView ( ) {
     
      
      
-     var i, countryData, countryName, html, bulletsHTML, tabsHTML, mapHTML, imageHTML, graphHTML, dataType;
+     var i, countryData, countryName, html, bulletsHTML, tabsHTML, mapHTML, imageHTML, graphHTML, dataType, graphs = [], graphObject;
      
      html = "";
      tabsHTML = "<option selected disabled>Select country</option>";
@@ -53,6 +53,7 @@ for (var d in dataset) {
    mapHTML = "";
    graphHTML = "";
    imageHTML = "";
+   graphObject = null;
 
  for ( i = 0; i < countryData.length; i++ ) {
         
@@ -72,7 +73,12 @@ for (var d in dataset) {
            break;
            
             case "graph" :
-                graphHTML = "";
+                if (graphObject == null) {
+                    graphObject = {};
+                    graphObject.id = d;
+                    graphObject.data = [];
+                }
+                graphObject.data.push( { value: countryData[i]["Value"], date: countryData[i]["Date"] });
            break;
            
            case "locator map" :
@@ -86,15 +92,23 @@ for (var d in dataset) {
         
 
     }
-    graphHTML = '<div class="country-graph"></div>';
+    
+    
+     if ( graphObject !== null ) {
+           graphs.push( graphObject );
+           graphHTML = '<div class="country-graph" id="graph-' + graphObject.id + '"></div>';
+     }
+     
+     
     bulletsHTML+= "</ul>";
     html += '<div class="country-graphics">' + imageHTML + graphHTML + '</div>'  + mapHTML + countryName + bulletsHTML + "</div>";
-
 }
     
    
    $("select").html(tabsHTML);
    $(".countries-container").html(html);
+   
+   buildGraphs( graphs );
    
    if ( selected !== null ) {
        $(".country-block").hide();
@@ -102,6 +116,88 @@ for (var d in dataset) {
        //$("select option[value='" + selected + "']").attr("selected","selected");
    }
 
+}
+
+function buildGraphs( graphData ) {
+    
+    var i;
+    
+    for ( i = 0; i < graphData.length; i++ ) {
+        buildGraph ( graphData[i] );
+    }
+    
+}
+
+function buildGraph ( graphData ) {
+    
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+//var parseDate = d3.time.format("%d-%b-%y").parse;
+
+var data = graphData.data;
+
+
+
+// var x = d3.time.scale()
+//     .range([0, width]);
+    
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+// var xAxis = d3.svg.axis()
+//     .scale(x)
+//     .orient("bottom");
+
+// var yAxis = d3.svg.axis()
+//     .scale(y)
+//     .orient("left");
+
+var area = d3.svg.area()
+    .x(function(d) { return x(d.date); })
+    .y0(height)
+    .y1(function(d) { return y(d.value); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  data.forEach(function(d, i) {
+    d.date = i; //parseDate(d.Date);
+    d.close = +d.value;
+  });
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "area")
+      .attr("d", area);
+
+//   svg.append("g")
+//       .attr("class", "x axis")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(xAxis);
+
+//   svg.append("g")
+//       .attr("class", "y axis")
+//       .call(yAxis)
+//     .append("text")
+//       .attr("transform", "rotate(-90)")
+//       .attr("y", 6)
+//       .attr("dy", ".71em")
+//       .style("text-anchor", "end")
+//       .text("Price ($)");
+
+    
 }
 
 function addListeners() {
