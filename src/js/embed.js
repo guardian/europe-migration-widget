8b748b98b152e0d7a4997836b119fde9ba72d05d
Garry Blight <garry.blight@guardian.co.uk>
2016-03-31 17:02:19 +0100

    More graph styling, font changes

--- src/js/embed.js
+++ src/js/embed.js
@@ -6,6 +6,7 @@ import embedHTML from './text/embed.html!text'
 
 var dataset = null;
 var selected = null;
+var expanded = false;
 
 window.init = function init(el, config) {
     iframeMessenger.enableAutoResize();
@@ -39,7 +40,7 @@ function buildView ( ) {
     
      
      
-     var i, countryData, countryName, html, bulletsHTML, tabsHTML, mapHTML, imageHTML, graphHTML, dataType, graphs = [], graphObject;
+     var i, countryData, countryName, html, bulletsHTML, tabsHTML, mapHTML, imageHTML, graphHTML, dataType, graphs = [], graphObject, graphTitle, bulletsTitle;
      
      html = "";
      tabsHTML = "<option selected disabled>Select country</option>";
@@ -54,6 +55,8 @@ for (var d in dataset) {
    graphHTML = "";
    imageHTML = "";
    graphObject = null;
+   graphTitle = "";
+   bulletsTitle = "";
 
  for ( i = 0; i < countryData.length; i++ ) {
         
@@ -81,6 +84,14 @@ for (var d in dataset) {
                 graphObject.data.push( { value: countryData[i]["Value"], date: countryData[i]["Date"] });
            break;
            
+            case "graph title" :
+                graphTitle = countryData[i]["Value"];
+           break;
+           
+           case "bullets title" :
+                bulletsTitle = '<h5 class="graph-title">' + countryData[i]["Value"] + '</h5>';
+           break;
+           
            case "locator map" :
                 mapHTML = '<div class="country-locator-map"></div>';
            break;
@@ -96,10 +107,10 @@ for (var d in dataset) {
     
      if ( graphObject !== null ) {
            graphs.push( graphObject );
-           graphHTML = '<div class="country-graph" id="graph-' + graphObject.id + '"><h5 class="graph-title">Weekly migrant arrivals</h5><div class="graph-inner"></div><div class="graph-readout"></div></div>';
+           graphHTML = '<div class="country-graph" id="graph-' + graphObject.id + '"><h5 class="graph-title">' + graphTitle + '<span></span></h5><div class="graph-inner"></div><div class="graph-readout"></div></div>';
      }
      
-     
+    bulletsHTML= bulletsTitle + bulletsHTML;
     bulletsHTML+= "</ul>";
     html += mapHTML + countryName + '<div class="country-graphics">' + imageHTML + graphHTML + '</div>' + bulletsHTML + "</div>";
 }
@@ -139,6 +150,7 @@ var data = graphData.data;
 var el ='#graph-' + graphData.id + ' .graph-inner';
 var parentEl ='#graph-' + graphData.id;
 
+
 var wP = 100;
 var wH = $(el).innerHeight() / $(el).innerWidth() * 100;
 
@@ -167,13 +179,21 @@ var y = d3.scale.linear()
 
 // y.domain(d3.extent(data, function(d) { return d.close; }));
 
+data.forEach(function(d, i) {
+    d.date = d.date; //parseDate(d.Date);
+    d.dateIndex = i;
+    d.value = +d.value;
+  });
+  
+  //console.log(data);
+
 var area = d3.svg.area()
-    .x(function(d) { return x(d.date); })
+    .x(function(d) { return x(d.dateIndex); })
     .y0(height)
     .y1(function(d) { return y(d.value); });
 
 var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
+    .x(function(d) { return x(d.dateIndex); })
     .y(function(d) { return y(d.value); });
 
 var svg = d3.select(el).append("svg")
@@ -186,13 +206,10 @@ var svg = d3.select(el).append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-  data.forEach(function(d, i) {
-    d.date = i; //parseDate(d.Date);
-    d.close = +d.value;
-  });
+  
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+  x.domain(d3.extent(data, function(d) { return d.dateIndex; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
   svg.append("path")
       .datum(data)
@@ -260,16 +277,25 @@ function updateReadout( data, el, mX, scaleY, w, h, parent ) {
     
     var oneDay = 100 / (data.length-1);
     var x = index * oneDay;
-   var y = (h - scaleY(data[index].value)) / h * 100;
+   var y = (h - scaleY(data[index].value)) / (h+1) * 100; // h+1 to accout for border;
     
     $(el).find(".circle-marker").css("left", x + "%").css("bottom", y + "%");
     $(el).find(".line-marker").css("left", x + "%").css("height", y + "%");
-    $(parent).find(".graph-readout").html(data[index].value);
+    $(parent).find(".graph-readout").html(addCommas(data[index].value));
+    $(parent).find(".graph-title span").html(", " + String(data[index].date).toLowerCase());
 }
 
 function addListeners() {
     $(".toggle-button").click( function (e) {
+        if (!expanded) {
         $(".country-block").show();
+        $(".toggle-button .button-text").html("Show less");
+        expanded = true;
+        } else {
+         expanded = false;
+         $(".country-block").hide();
+        $(".toggle-button .button-text").html("Show more countries");   
+        }
     });
     
     $("#country-select").change(function() {
@@ -290,4 +316,17 @@ function getParameter(paramName) {
     }
   }
   return null;
+}
+
+function addCommas(nStr) {
+    nStr += '';
+    
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
 }
